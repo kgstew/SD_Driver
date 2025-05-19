b4147f2ee7fc8a3d7f5fe8f3d433927c31dbc99a
Kyle Stewart <kgstew@gmail.com>
2025-05-19 17:54:53 -0600

    Simple Blinking LED

--- Src/main.c
+++ Src/main.c
@@ -1,5 +1,20 @@
-int main() {
-	while(1) {
+#include "stm32f4xx.h"
 
-	}
+#define GPIOAEN (1U << 0)
+#define PIN5 (1U << 5)
+#define LED_PIN PIN5
+
+int main()
+{
+    /* Enable Clock Access to GPIOA */
+    RCC->AHB1ENR |= GPIOAEN;
+
+    /* Set PA5 to Output mode */
+    GPIOA->MODER |= (1U << 10);
+    GPIOA->MODER &= (1U << 11);
+
+    while (1) {
+        GPIOA->ODR ^= LED_PIN;
+        for (int i = 0; i < 100000; i++) { }
+    }
 }
